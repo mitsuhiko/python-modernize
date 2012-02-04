9299cf05f90432b3bfc448b543b1afc6713fa1b2
Armin Ronacher <armin.ronacher@active-4.com>
2012-02-04 19:09:00 +0100

    Added command line script to setup.py and fixed a few oddities there.

--- setup.py
+++ setup.py
@@ -8,11 +8,16 @@ setup(
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
     version='0.1',
-    url='http://github.com/mitsuhiko/modernize',
-    packages=['modernize', 'modernize.fixes'],
+    url='http://github.com/mitsuhiko/python-modernize',
+    packages=['libmodernize', 'libmodernize.fixes'],
     description='A hack on top of 2to3 for modernizing code for '
                 'hybrid codebases.',
     long_description=readme,
+    entry_points={
+        'console_scripts': [
+            'python-modernize = libmodernize.main:main'
+        ]
+    },
     zip_safe=False,
     test_suite='tests',
     classifiers=[
