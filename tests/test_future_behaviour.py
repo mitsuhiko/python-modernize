dd36c8f2ffe5d4ab8169a02027ab6d5e3932d438
Daira Hopwood <daira@jacaranda.org>
2013-12-21 12:15:30 +0000

    Merge tests from branch 'multiple-futures-fix' of https://github.com/Mekk/python-modernize into HEAD
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

---
+++ tests/test_future_behaviour.py
@@ -0,0 +1,94 @@
+
+# Tests for problem with multiple futures added to single file
+
+import tempfile
+import os
+import shutil
+from libmodernize.main import main as modernize_main
+
+SINGLE_PRINT_CONTENT = """
+print 'world'
+"""
+
+TWO_PRINTS_CONTENT = """
+print 'Hello'
+print 'world'
+"""
+
+COMPLICATED_CONTENT = """
+print 'Hello'
+print u'world'
+def sub(x):
+    print x, u"here"
+"""
+
+PROBLEMATIC_CONTENT = '''
+
+"""
+Hello
+"""
+
+from a.b.c import d
+
+def test_existing():
+    print d
+'''
+
+def _check_for_multiple_futures(file_name, source_content):
+    """
+    Checks for multiple identical futures in given file,
+    raises if found.
+    Returns dictionary of found futures (name => 1)
+    """
+    counts = {}
+    result_content = ""
+    with open(file_name, "rt") as input:
+        for line in input:
+            if line.startswith("from __future__"):
+                counts[line] = 1 + counts.get(line, 0)
+            result_content += line
+    for future, how_many in counts.items():
+        if how_many > 1:
+            raise Exception("The same future repeated more than once ({0} times):\n{1}\n\n* Input file:\n{2}\n\n* Output file:\n{3}\n".format(how_many, future, source_content, result_content))
+    return counts
+
+def _check_on_input(file_content, extra_flags = []):
+    try:
+        tmpdirname = tempfile.mkdtemp()
+        test_input_name = os.path.join(tmpdirname, "input.py")
+        with open(test_input_name, "wt") as input:
+            input.write(file_content)
+        modernize_main(extra_flags + ["-w", test_input_name])
+        _check_for_multiple_futures(test_input_name, file_content)
+    finally:
+        shutil.rmtree(tmpdirname)
+
+def test_single_print():
+    _check_on_input(SINGLE_PRINT_CONTENT)
+
+def test_two_prints():
+    _check_on_input(TWO_PRINTS_CONTENT)
+
+def test_many_prints_and_unicode():
+    _check_on_input(COMPLICATED_CONTENT, ["--future-unicode"])
+
+def test_two_files_on_single_run():
+    # Mostly to test whether second file gets it's from future
+    try:
+        tmpdirname = tempfile.mkdtemp()
+        input_names = [ os.path.join(tmpdirname, "input_{0}.py".format(idx))
+                        for idx in range(0,3) ]
+        for input_name in input_names:
+            with open(input_name, "wt") as input:
+                input.write(TWO_PRINTS_CONTENT)
+        modernize_main(["-w"] + input_names)
+        for input_name in input_names:
+            futs = _check_for_multiple_futures(input_name, TWO_PRINTS_CONTENT)
+            if not futs:
+                raise Exception("File {0} got no from __future__ (but it should)")
+    finally:
+        shutil.rmtree(tmpdirname)
+
+def test_problematic_file():
+    # ON this one I get crash
+    _check_on_input(PROBLEMATIC_CONTENT)
