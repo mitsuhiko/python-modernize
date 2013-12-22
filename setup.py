f40021c465784f16f246f7f27d5a4781f4f4a36f
Daira Hopwood <daira@jacaranda.org>
2013-12-22 17:03:35 +0000

    Add Python 2.6 and 2.7 to PyPI metadata.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: PHP',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 3'
     ]
 )
