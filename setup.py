43f58cb57bd9206538432b2340a63a3db640ba98
Daira Hopwood <daira@jacaranda.org>
2014-07-26 20:52:08 +0100

    python-modernize doesn't *run* on Python 3, yet. (Yes, I know about the modernize3k fork.)
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 3'
+        # does not run *on* Python 3, yet
     ]
 )
