345e257109dccfbbed585b7db72297fa9f8d28ed
Thomas Kluyver <takowl@gmail.com>
2014-08-08 12:19:18 -0700

    Add tests for converting unicode literals

---
+++ tests/test_fix_unicode.py
@@ -0,0 +1,46 @@
+from utils import check_on_input
+
+UNICODE_LITERALS = """\
+a = u''
+b = U"\\u2041"
+c = ur'''blah
+foo'''
+"""
+
+UNICODE_LITERALS_six = """\
+import six
+a = six.u('')
+b = six.u("\\u2041")
+c = six.u(r'''blah
+foo''')
+"""
+
+UNICODE_LITERALS_compat = UNICODE_LITERALS
+
+UNICODE_LITERALS_future = """\
+from __future__ import unicode_literals
+a = ''
+b = "\\u2041"
+c = r'''blah
+foo'''
+"""
+
+def test_unicode_six():
+    check_on_input(UNICODE_LITERALS, UNICODE_LITERALS_six)
+
+def test_unicode_compat():
+    check_on_input(UNICODE_LITERALS, UNICODE_LITERALS_compat, extra_flags=['--compat-unicode'])
+
+def test_unicode_future():
+    check_on_input(UNICODE_LITERALS, UNICODE_LITERALS_future, extra_flags=['--future-unicode'])
+
+UNICODE_NAME = ("""\
+unicode(x)
+""", """\
+import six
+six.text_type(x)
+"""
+)
+
+def test_unicode_name():
+    check_on_input(*UNICODE_NAME)
