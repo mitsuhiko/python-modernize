f3701257aee0f463aad24efa9ea5a8a945fca9b5
Thomas Kluyver <takowl@gmail.com>
2014-08-11 09:53:54 -0700

    Remove spurious PHP classifier

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     test_suite='nose.collector',
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: PHP',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2.6',
