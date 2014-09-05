5a2239df6b4c58b461d8fe4f121711f5f99baae2
Brett Cannon <brett@python.org>
2014-09-05 10:58:32 -0400

    Rename a test to make its purpose more obvious

--- tests/test_fix_map.py
+++ tests/test_fix_map.py
@@ -1,12 +1,6 @@
 from utils import check_on_input
 
 
-MAP_1_ARG = ("""\
-map(x)
-""", """\
-map(x)
-""")
-
 MAP_2_ARGS = ("""\
 map(x, [1])
 """, """\
@@ -28,6 +22,12 @@ from six.moves import map
 map(x, [1], [2], [3])
 """)
 
+MAP_TOO_FEW_ARGS = ("""\
+map(x)
+""", """\
+map(x)
+""")
+
 MAP_KWARGS = ("""\
 map(function=x, [1])
 """, """\
@@ -41,9 +41,6 @@ x = map
 """)
 
 
-def test_map_1_arg():
-    check_on_input(*MAP_1_ARG)
-
 def test_map_2_args():
     check_on_input(*MAP_2_ARGS)
 
@@ -52,6 +49,9 @@ def test_map_3_args():
 
 def test_map_4_args():
     check_on_input(*MAP_4_ARGS)
+
+def test_map_too_few_args():
+    check_on_input(*MAP_TOO_FEW_ARGS)
 
 def test_map_kwargs():
     check_on_input(*MAP_KWARGS)
