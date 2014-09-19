c4405e6fa99811678f1b861c6a49e22079ba0d70
Brett Cannon <brett@python.org>
2014-09-19 14:38:52 -0400

    Bump the priority of fix_import
    Guarantees it does its thing before any other fixer accidentally blocks
    its execution.

--- libmodernize/fixes/fix_import.py
+++ libmodernize/fixes/fix_import.py
@@ -6,6 +6,10 @@ import libmodernize
 
 class FixImport(fix_import.FixImport):
 
+    # Make sure this runs before any other fixer to guarantee that any other
+    # added absolute_import doesn't block this fixer's execution.
+    run_order = 1
+
     def transform(self, node, results):
         results = super(FixImport, self).transform(node, results)
         if results is None:
