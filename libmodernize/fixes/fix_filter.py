223f40be53c74f795a6cd3b69a17dcc40735becb
Brett Cannon <brett@python.org>
2014-09-19 09:41:23 -0400

    Simplify fix_filter
    Part of #72

--- libmodernize/fixes/fix_filter.py
+++ libmodernize/fixes/fix_filter.py
@@ -1,42 +1,17 @@
 # Copyright 2008 Armin Ronacher.
 # Licensed to PSF under a Contributor Agreement.
 
-from lib2to3 import fixer_base
-from lib2to3.fixer_util import Call, Name, touch_import, in_special_context
+from lib2to3 import fixer_util
+from lib2to3.fixes import fix_filter
 
 
-class FixFilter(fixer_base.ConditionalFix):
+class FixFilter(fix_filter.FixFilter):
 
-    BM_compatible = True
-    order = "pre"
     skip_on = "six.moves.filter"
 
-    PATTERN = """
-    power< 'filter'
-        trailer< '('
-            arglist< (
-                not(argument<any '=' any>) any ','
-                not(argument<any '=' any>) any
-            ) >
-        ')' >
-    >
-    """
-
     def transform(self, node, results):
-        if self.should_skip(node):
-            # Make the fixer idempotent - if six.moves.filter is already imported,
-            # skip it. should_skip() caches the state the first time we check,
-            # so it doesn't skip after *we've* added the six.moves import.
-            return
-
-        touch_import(u'six.moves', u'filter', node)
-        if in_special_context(node):
-            # The node is somewhere where it only needs to be an iterator,
-            # e.g. a for loop - don't wrap in list()
-            return
-
-        new = node.clone()
-        new.prefix = ""
-        new = Call(Name("list"), [new])
-        new.prefix = node.prefix
-        return new
+        result = super(FixFilter, self).transform(node, results)
+        # Keep performance improvement from six.moves.filter in iterator
+        # contexts on Python 2.7.
+        fixer_util.touch_import(u'six.moves', u'filter', node)
+        return result
