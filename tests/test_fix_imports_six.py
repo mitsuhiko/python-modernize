bdd46afd2ea4e1813aaff20568574a4ed2214453
Thomas Kluyver <takowl@gmail.com>
2014-09-26 13:12:01 -0700

    Fix test for six imports mapping

--- tests/test_fix_imports_six.py
+++ tests/test_fix_imports_six.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import sys
+import unittest
 
 try:
     from six.moves import tkinter
@@ -35,13 +36,20 @@ def test_moved_module():
 def test_moved_module_fromlist():
     check_on_input(*MOVED_MODULE_FROMLIST)
 
+@unittest.skipIf(sys.version_info[0] >= 3, "Test only runs on Python 2")
 def test_validate_mapping():
     for py2_name, six_name in fix_imports_six.FixImportsSix.mapping.items():
         try:
             __import__(py2_name)
             __import__(six_name)
-        except ImportError as exc:
-            if 'tkinter' in six_name and tkinter is not None:
-                raise
-            elif 'winreg' in six_name and sys.platform.startswith('win'):
+        except ImportError:
+            if 'tkinter' in six_name:
+                # Ignore error if tkinter not installed
+                if tkinter is not None:
+                    raise
+            elif 'winreg' in six_name:
+                # Ignore error if we're not on Windows
+                if sys.platform.startswith('win'):
+                    raise
+            else:
                 raise
